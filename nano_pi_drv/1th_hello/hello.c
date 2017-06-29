0b4bdbe8e0f1b8b17a84784bead7268c76425932
hceng <huangcheng.job@foxmail.com>
2017-06-29 15:33:01 +0800

    msi

---
+++ nano_pi_drv/1th_hello/hello.c
@@ -0,0 +1,19 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+
+static int hello_init(void)
+{
+  printk("Hello, world.\n");
+  return 0;
+}
+
+static void hello_exit(void)
+{
+  printk("Hello, exit.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("Dual BSD/GPL");
---
+++ nano_pi_drv/2th_led/firstdrvtest.c
@@ -0,0 +1,38 @@
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+/* firstdrvtest on
+  * firstdrvtest off
+  */
+int main(int argc, char **argv)
+{
+	int fd;
+	int val = 1;
+	fd = open("/dev/xyz", O_RDWR);
+	if (fd < 0)
+	{
+		printf("can't open!\n");
+	}
+	if (argc != 2)
+	{
+		printf("Usage :\n");
+		printf("%s <on|off>\n", argv[0]);
+		return 0;
+	}
+
+	if (strcmp(argv[1], "on") == 0)
+	{
+			printf("on\n");
+	}
+	else
+	{
+	    printf("no\n");
+	}
+	
+	write(fd, &val, 4);
+ 
+	return 0;
+}
