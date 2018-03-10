fc6e0b4bd6ee5e0a0c53672e8f5124466ec9a884
hceng <huangcheng.job@foxmail.com>
2018-03-10 15:47:49 +0800

    auto

--- tiny4412/07_vivid/my_vivid/fillbuf.c
+++ tiny4412/07_vivid/my_vivid/fillbuf.c
@@ -2,15 +2,16 @@
 static struct v4l2_format my_vivid_format;
 
 /* Bars and Colors should match positions */
-enum colors {
-	WHITE,
-	AMBAR,
-	CYAN,
-	GREEN,
-	MAGENTA,
-	RED,
-	BLUE,
-	BLACK,
+enum colors
+{
+    WHITE,
+    AMBAR,
+    CYAN,
+    GREEN,
+    MAGENTA,
+    RED,
+    BLUE,
+    BLACK,
 };
 
 /* R   G   B */
@@ -23,57 +24,59 @@ enum colors {
 #define COLOR_BLUE	{  0,   0, 255}
 #define COLOR_BLACK	{  0,   0,   0}
 
-struct bar_std {
-	u8 bar[8][3];
+struct bar_std
+{
+    u8 bar[8][3];
 };
 
 /* Maximum number of bars are 10 - otherwise, the input print code should be modified */
-static struct bar_std bars[] = {
-	{	/* Standard ITU-R color bar sequence */
-		{
-			COLOR_WHITE,
-			COLOR_AMBAR,
-			COLOR_CIAN,
-			COLOR_GREEN,
-			COLOR_MAGENTA,
-			COLOR_RED,
-			COLOR_BLUE,
-			COLOR_BLACK,
-		}
-	}, {
-		{
-			COLOR_WHITE,
-			COLOR_AMBAR,
-			COLOR_BLACK,
-			COLOR_WHITE,
-			COLOR_AMBAR,
-			COLOR_BLACK,
-			COLOR_WHITE,
-			COLOR_AMBAR,
-		}
-	}, {
-		{
-			COLOR_WHITE,
-			COLOR_CIAN,
-			COLOR_BLACK,
-			COLOR_WHITE,
-			COLOR_CIAN,
-			COLOR_BLACK,
-			COLOR_WHITE,
-			COLOR_CIAN,
-		}
-	}, {
-		{
-			COLOR_WHITE,
-			COLOR_GREEN,
-			COLOR_BLACK,
-			COLOR_WHITE,
-			COLOR_GREEN,
-			COLOR_BLACK,
-			COLOR_WHITE,
-			COLOR_GREEN,
-		}
-	},
+static struct bar_std bars[] =
+{
+    {	/* Standard ITU-R color bar sequence */
+        {
+            COLOR_WHITE,
+            COLOR_AMBAR,
+            COLOR_CIAN,
+            COLOR_GREEN,
+            COLOR_MAGENTA,
+            COLOR_RED,
+            COLOR_BLUE,
+            COLOR_BLACK,
+        }
+    }, {
+        {
+            COLOR_WHITE,
+            COLOR_AMBAR,
+            COLOR_BLACK,
+            COLOR_WHITE,
+            COLOR_AMBAR,
+            COLOR_BLACK,
+            COLOR_WHITE,
+            COLOR_AMBAR,
+        }
+    }, {
+        {
+            COLOR_WHITE,
+            COLOR_CIAN,
+            COLOR_BLACK,
+            COLOR_WHITE,
+            COLOR_CIAN,
+            COLOR_BLACK,
+            COLOR_WHITE,
+            COLOR_CIAN,
+        }
+    }, {
+        {
+            COLOR_WHITE,
+            COLOR_GREEN,
+            COLOR_BLACK,
+            COLOR_WHITE,
+            COLOR_GREEN,
+            COLOR_BLACK,
+            COLOR_WHITE,
+            COLOR_GREEN,
+        }
+    },
 };
 
 #define NUM_INPUTS ARRAY_SIZE(bars)
@@ -92,193 +95,208 @@ static unsigned char myvivi_cur_bars[8][3];
 /* precalculate color bar values to speed up rendering */
 static void my_vivid_precalculate_bars(int input)
 {
-	unsigned char r, g, b;
-	int k, is_yuv;
+    unsigned char r, g, b;
+    int k, is_yuv;
 
 
-	for (k = 0; k < 8; k++) {
-		r = bars[input].bar[k][0];
-		g = bars[input].bar[k][1];
-		b = bars[input].bar[k][2];
-		is_yuv = 0;
+    for (k = 0; k < 8; k++)
+    {
+        r = bars[input].bar[k][0];
+        g = bars[input].bar[k][1];
+        b = bars[input].bar[k][2];
+        is_yuv = 0;
 
-		switch (my_vivid_format.fmt.pix.pixelformat) {
-		case V4L2_PIX_FMT_YUYV:
-		case V4L2_PIX_FMT_UYVY:
-			is_yuv = 1;
-			break;
-		case V4L2_PIX_FMT_RGB565:
-		case V4L2_PIX_FMT_RGB565X:
-			r >>= 3;
-			g >>= 2;
-			b >>= 3;
-			break;
-		case V4L2_PIX_FMT_RGB555:
-		case V4L2_PIX_FMT_RGB555X:
-			r >>= 3;
-			g >>= 3;
-			b >>= 3;
-			break;
-		}
+        switch (my_vivid_format.fmt.pix.pixelformat)
+        {
+        case V4L2_PIX_FMT_YUYV:
+        case V4L2_PIX_FMT_UYVY:
+            is_yuv = 1;
+            break;
+        case V4L2_PIX_FMT_RGB565:
+        case V4L2_PIX_FMT_RGB565X:
+            r >>= 3;
+            g >>= 2;
+            b >>= 3;
+            break;
+        case V4L2_PIX_FMT_RGB555:
+        case V4L2_PIX_FMT_RGB555X:
+            r >>= 3;
+            g >>= 3;
+            b >>= 3;
+            break;
+        }
 
-		if (is_yuv) {
-			myvivi_cur_bars[k][0] = TO_Y(r, g, b);	/* Luma */
-			myvivi_cur_bars[k][1] = TO_U(r, g, b);	/* Cb */
-			myvivi_cur_bars[k][2] = TO_V(r, g, b);	/* Cr */
-		} else {
-			myvivi_cur_bars[k][0] = r;
-			myvivi_cur_bars[k][1] = g;
-			myvivi_cur_bars[k][2] = b;
-		}
-	}
+        if (is_yuv)
+        {
+            myvivi_cur_bars[k][0] = TO_Y(r, g, b);	/* Luma */
+            myvivi_cur_bars[k][1] = TO_U(r, g, b);	/* Cb */
+            myvivi_cur_bars[k][2] = TO_V(r, g, b);	/* Cr */
+        }
+        else
+        {
+            myvivi_cur_bars[k][0] = r;
+            myvivi_cur_bars[k][1] = g;
+            myvivi_cur_bars[k][2] = b;
+        }
+    }
 
 }
 
 
 static void my_vivid_gen_twopix(unsigned char *buf, int colorpos)
 {
-	unsigned char r_y, g_u, b_v;
-	unsigned char *p;
-	int color;
+    unsigned char r_y, g_u, b_v;
+    unsigned char *p;
+    int color;
 
-	printk("enter %s\n", __func__);
-	
-	r_y = myvivi_cur_bars[colorpos][0]; /* R or precalculated Y */
-	g_u = myvivi_cur_bars[colorpos][1]; /* G or precalculated U */
-	b_v = myvivi_cur_bars[colorpos][2]; /* B or precalculated V */
+    printk("enter %s\n", __func__);
 
-	for (color = 0; color < 4; color++) {
-		p = buf + color;
+    r_y = myvivi_cur_bars[colorpos][0]; /* R or precalculated Y */
+    g_u = myvivi_cur_bars[colorpos][1]; /* G or precalculated U */
+    b_v = myvivi_cur_bars[colorpos][2]; /* B or precalculated V */
 
-		switch (my_vivid_format.fmt.pix.pixelformat) {
-		case V4L2_PIX_FMT_YUYV:
-			switch (color) {
-			case 0:
-			case 2:
-				*p = r_y;
-				break;
-			case 1:
-				*p = g_u;
-				break;
-			case 3:
-				*p = b_v;
-				break;
-			}
-			break;
-		case V4L2_PIX_FMT_UYVY:
-			switch (color) {
-			case 1:
-			case 3:
-				*p = r_y;
-				break;
-			case 0:
-				*p = g_u;
-				break;
-			case 2:
-				*p = b_v;
-				break;
-			}
-			break;
-		case V4L2_PIX_FMT_RGB565:
-			switch (color) {
-			case 0:
-			case 2:
-				*p = (g_u << 5) | b_v;
-				break;
-			case 1:
-			case 3:
-				*p = (r_y << 3) | (g_u >> 3);
-				break;
-			}
-			break;
-		case V4L2_PIX_FMT_RGB565X:
-			switch (color) {
-			case 0:
-			case 2:
-				*p = (r_y << 3) | (g_u >> 3);
-				break;
-			case 1:
-			case 3:
-				*p = (g_u << 5) | b_v;
-				break;
-			}
-			break;
-		case V4L2_PIX_FMT_RGB555:
-			switch (color) {
-			case 0:
-			case 2:
-				*p = (g_u << 5) | b_v;
-				break;
-			case 1:
-			case 3:
-				*p = (r_y << 2) | (g_u >> 3);
-				break;
-			}
-			break;
-		case V4L2_PIX_FMT_RGB555X:
-			switch (color) {
-			case 0:
-			case 2:
-				*p = (r_y << 2) | (g_u >> 3);
-				break;
-			case 1:
-			case 3:
-				*p = (g_u << 5) | b_v;
-				break;
-			}
-			break;
-		}
-	}
+    for (color = 0; color < 4; color++)
+    {
+        p = buf + color;
+
+        switch (my_vivid_format.fmt.pix.pixelformat)
+        {
+        case V4L2_PIX_FMT_YUYV:
+            switch (color)
+            {
+            case 0:
+            case 2:
+                *p = r_y;
+                break;
+            case 1:
+                *p = g_u;
+                break;
+            case 3:
+                *p = b_v;
+                break;
+            }
+            break;
+        case V4L2_PIX_FMT_UYVY:
+            switch (color)
+            {
+            case 1:
+            case 3:
+                *p = r_y;
+                break;
+            case 0:
+                *p = g_u;
+                break;
+            case 2:
+                *p = b_v;
+                break;
+            }
+            break;
+        case V4L2_PIX_FMT_RGB565:
+            switch (color)
+            {
+            case 0:
+            case 2:
+                *p = (g_u << 5) | b_v;
+                break;
+            case 1:
+            case 3:
+                *p = (r_y << 3) | (g_u >> 3);
+                break;
+            }
+            break;
+        case V4L2_PIX_FMT_RGB565X:
+            switch (color)
+            {
+            case 0:
+            case 2:
+                *p = (r_y << 3) | (g_u >> 3);
+                break;
+            case 1:
+            case 3:
+                *p = (g_u << 5) | b_v;
+                break;
+            }
+            break;
+        case V4L2_PIX_FMT_RGB555:
+            switch (color)
+            {
+            case 0:
+            case 2:
+                *p = (g_u << 5) | b_v;
+                break;
+            case 1:
+            case 3:
+                *p = (r_y << 2) | (g_u >> 3);
+                break;
+            }
+            break;
+        case V4L2_PIX_FMT_RGB555X:
+            switch (color)
+            {
+            case 0:
+            case 2:
+                *p = (r_y << 2) | (g_u >> 3);
+                break;
+            case 1:
+            case 3:
+                *p = (g_u << 5) | b_v;
+                break;
+            }
+            break;
+        }
+    }
 }
 
 static void my_vivid_gen_line(char *basep, int inipos, int wmax,
-		int hmax, int line, int count)
+                              int hmax, int line, int count)
 {
-	int  w;
-	int pos = inipos;
+    int  w;
+    int pos = inipos;
 
-	printk("enter %s\n", __func__);
-	
-	/* We will just duplicate the second pixel at the packet */
-	wmax /= 2;
+    printk("enter %s\n", __func__);
 
-	/* Generate a standard color bar pattern */
-	for (w = 0; w < wmax; w++) {
-		int colorpos = ((w + count) * 8/(wmax + 1)) % 8;
+    /* We will just duplicate the second pixel at the packet */
+    wmax /= 2;
 
-		my_vivid_gen_twopix(basep + pos, colorpos);
-		pos += 4; /* only 16 bpp supported for now */
-	}
-	return;
+    /* Generate a standard color bar pattern */
+    for (w = 0; w < wmax; w++)
+    {
+        int colorpos = ((w + count) * 8 / (wmax + 1)) % 8;
+
+        my_vivid_gen_twopix(basep + pos, colorpos);
+        pos += 4; /* only 16 bpp supported for now */
+    }
+    return;
 }
 
 static void my_vivid_fillbuff(struct videobuf_buffer *vb)
 {
-	int h , pos = 0;
-	int hmax  = vb->height;
-	int wmax  = vb->width;
-	char *tmpbuf;
-	void *vbuf = videobuf_to_vmalloc(vb);
+    int h , pos = 0;
+    int hmax  = vb->height;
+    int wmax  = vb->width;
+    char *tmpbuf;
+    void *vbuf = videobuf_to_vmalloc(vb);
     static int mv_count = 0;
 
-	printk("enter %s\n", __func__);
-	
-	if (!vbuf)
-		return;
+    printk("enter %s\n", __func__);
 
-	tmpbuf = kmalloc(wmax * 2, GFP_ATOMIC);
-	if (!tmpbuf)
-		return;
+    if (!vbuf)
+        return;
 
-	for (h = 0; h < hmax; h++) {
-		my_vivid_gen_line(tmpbuf, 0, wmax, hmax, h, mv_count);
-		memcpy(vbuf + pos, tmpbuf, wmax * 2);
-		pos += wmax*2;
-	}
+    tmpbuf = kmalloc(wmax * 2, GFP_ATOMIC);
+    if (!tmpbuf)
+        return;
 
-	mv_count++;
+    for (h = 0; h < hmax; h++)
+    {
+        my_vivid_gen_line(tmpbuf, 0, wmax, hmax, h, mv_count);
+        memcpy(vbuf + pos, tmpbuf, wmax * 2);
+        pos += wmax * 2;
+    }
 
-	kfree(tmpbuf);
+    mv_count++;
+
+    kfree(tmpbuf);
 }
 
 
--- tiny4412/07_vivid/my_vivid/my_vivid.c
+++ tiny4412/07_vivid/my_vivid/my_vivid.c
@@ -31,32 +31,33 @@ static struct timer_list my_vivid_timer;
 static void my_vivid_timer_function(unsigned long data)
 {
     struct videobuf_buffer *vb;
-	void *vbuf;
-	struct timeval ts;
-    
-	printk("enter %s\n", __func__);
-    
+    void *vbuf;
+    struct timeval ts;
+
+    printk("enter %s\n", __func__);
+
     /* 1. 构造数据: 从队列头部取出第1个videobuf, 填充数据 */
 
     /* 1.1 从本地队列取出第1个videobuf */
-    if (list_empty(&my_vivid_vb_local_queue)) {
+    if (list_empty(&my_vivid_vb_local_queue))
+    {
         goto out;
     }
-    
+
     vb = list_entry(my_vivid_vb_local_queue.next,
-             struct videobuf_buffer, queue);
-    
+                    struct videobuf_buffer, queue);
+
     /* Nobody is waiting on this buffer, return */
     if (!waitqueue_active(&vb->done))
         goto out;
-    
+
 
     /* 1.2 填充数据 */
     vbuf = videobuf_to_vmalloc(vb);
     //memset(vbuf, 0xFF, vb->size);
 
     my_vivid_fillbuff(vb);
-    
+
     vb->field_count++;
     do_gettimeofday(&ts);
     vb->ts = ts;
@@ -67,17 +68,17 @@ static void my_vivid_timer_function(unsigned long data)
 
     /* 2. 唤醒进程: 唤醒videobuf->done上的进程 */
     wake_up(&vb->done);
-    
+
 out:
     /* 3. 修改timer的超时时间 : 30fps, 1秒里有30帧数据
      *    每1/30 秒产生一帧数据
      */
-    mod_timer(&my_vivid_timer, jiffies + HZ/30);
+    mod_timer(&my_vivid_timer, jiffies + HZ / 30);
 }
 
 
 /* 参考documentations/video4linux/v4l2-framework.txt:
-       drivers\media\video\videobuf-core.c 
+       drivers\media\video\videobuf-core.c
  ops->buf_setup   - calculates the size of the video buffers and avoid they to waste more than some maximum limit of RAM;
  ops->buf_prepare - fills the video buffer structs and calls videobuf_iolock() to alloc and prepare mmaped memory;
  ops->buf_queue   - advices the driver that another buffer were requested (by read() or by QBUF);
@@ -89,36 +90,36 @@ out:
 //APP调用ioctl VIDIOC_REQBUFS时会导致此函数被调用,它重新调整count和size
 static int my_vivid_buffer_setup(struct videobuf_queue *vq, unsigned int *count, unsigned int *size)
 {
-	printk("enter %s\n", __func__);
-	
-	*size = my_vivid_format.fmt.pix.sizeimage;
+    printk("enter %s\n", __func__);
 
-	if (0 == *count)
-		*count = 32;
+    *size = my_vivid_format.fmt.pix.sizeimage;
 
-	return 0;
+    if (0 == *count)
+        *count = 32;
+
+    return 0;
 }
 
 //APP调用ioctlVIDIOC_QBUF时导致此函数被调用,它会填充video_buffer结构体并调用videobuf_iolock来分配内存
 static int my_vivid_buffer_prepare(struct videobuf_queue *vq, struct videobuf_buffer *vb,
-						enum v4l2_field field)
+                                   enum v4l2_field field)
 {
-	printk("enter %s\n", __func__);
-	
+    printk("enter %s\n", __func__);
+
     /* 1. 设置videobuf */
-	vb->size         = my_vivid_format.fmt.pix.sizeimage;
+    vb->size         = my_vivid_format.fmt.pix.sizeimage;
     vb->bytesperline = my_vivid_format.fmt.pix.bytesperline;
-	vb->width        = my_vivid_format.fmt.pix.width;
-	vb->height       = my_vivid_format.fmt.pix.height;
-	vb->field        = field;
-    
+    vb->width        = my_vivid_format.fmt.pix.width;
+    vb->height       = my_vivid_format.fmt.pix.height;
+    vb->field        = field;
+
     /* 2. 做些准备工作 */
     my_vivid_precalculate_bars(0);
 
     /* 3. 设置状态 */
-	vb->state = VIDEOBUF_PREPARED;
+    vb->state = VIDEOBUF_PREPARED;
 
-	return 0;
+    return 0;
 }
 
 
@@ -129,9 +130,9 @@ static int my_vivid_buffer_prepare(struct videobuf_queue *vq, struct videobuf_bu
  */
 static void my_vivid_buffer_queue(struct videobuf_queue *vq, struct videobuf_buffer *vb)
 {
-	printk("enter %s\n", __func__);
-	
-	vb->state = VIDEOBUF_QUEUED;
+    printk("enter %s\n", __func__);
+
+    vb->state = VIDEOBUF_QUEUED;
 
     /* 把videobuf放入本地一个队列尾部
      * 定时器处理函数就可以从本地队列取出videobuf
@@ -141,20 +142,21 @@ static void my_vivid_buffer_queue(struct videobuf_queue *vq, struct videobuf_buf
 
 /* APP不再使用队列时, 用它来释放内存 */
 static void my_vivid_buffer_release(struct videobuf_queue *vq,
-			   struct videobuf_buffer *vb)
+                                    struct videobuf_buffer *vb)
 {
-	printk("enter %s\n", __func__);
-	
-	videobuf_vmalloc_free(vb);
-    
-	vb->state = VIDEOBUF_NEEDS_INIT;
+    printk("enter %s\n", __func__);
+
+    videobuf_vmalloc_free(vb);
+
+    vb->state = VIDEOBUF_NEEDS_INIT;
 }
 
-static struct videobuf_queue_ops my_vivid_video_qops = {
-	.buf_setup      = my_vivid_buffer_setup, /* 计算大小以免浪费 */
-	.buf_prepare    = my_vivid_buffer_prepare,
-	.buf_queue      = my_vivid_buffer_queue,
-	.buf_release    = my_vivid_buffer_release,
+static struct videobuf_queue_ops my_vivid_video_qops =
+{
+    .buf_setup      = my_vivid_buffer_setup, /* 计算大小以免浪费 */
+    .buf_prepare    = my_vivid_buffer_prepare,
+    .buf_queue      = my_vivid_buffer_queue,
+    .buf_release    = my_vivid_buffer_release,
 };
 
 
@@ -164,9 +166,9 @@ static int my_vivid_open(struct file *file)
     printk("enter %s\n", __func__);
 
     //队列操作c:初始化
-	videobuf_queue_vmalloc_init(&my_vivid_vb_vidqueue, &my_vivid_video_qops,
-			NULL, &my_vivid_queue_slock, V4L2_BUF_TYPE_VIDEO_CAPTURE, V4L2_FIELD_INTERLACED,
-			sizeof(struct videobuf_buffer), NULL, NULL); /* 倒数第3个参数是buffer的头部大小 */
+    videobuf_queue_vmalloc_init(&my_vivid_vb_vidqueue, &my_vivid_video_qops,
+                                NULL, &my_vivid_queue_slock, V4L2_BUF_TYPE_VIDEO_CAPTURE, V4L2_FIELD_INTERLACED,
+                                sizeof(struct videobuf_buffer), NULL, NULL); /* 倒数第3个参数是buffer的头部大小 */
 
     my_vivid_timer.expires = jiffies + 1;
     add_timer(&my_vivid_timer);
@@ -176,195 +178,197 @@ static int my_vivid_open(struct file *file)
 
 static int my_vivid_close(struct file *file)
 {
-	printk("enter %s\n", __func__);
-	
+    printk("enter %s\n", __func__);
+
     del_timer(&my_vivid_timer);
-	videobuf_stop(&my_vivid_vb_vidqueue);
-	videobuf_mmap_free(&my_vivid_vb_vidqueue);
-    
-	return 0;
+    videobuf_stop(&my_vivid_vb_vidqueue);
+    videobuf_mmap_free(&my_vivid_vb_vidqueue);
+
+    return 0;
 }
 
 static int my_vivid_mmap(struct file *file, struct vm_area_struct *vma)
 {
-	printk("enter %s\n", __func__);
-	
+    printk("enter %s\n", __func__);
+
     return videobuf_mmap_mapper(&my_vivid_vb_vidqueue, vma);
 }
 
 static unsigned int my_vivid_poll(struct file *file, struct poll_table_struct *wait)
 {
-	printk("enter %s\n", __func__);
-    
-	return videobuf_poll_stream(file, &my_vivid_vb_vidqueue, wait);
+    printk("enter %s\n", __func__);
+
+    return videobuf_poll_stream(file, &my_vivid_vb_vidqueue, wait);
 }
 
 
 /* v4l2_ioctl_ops */
 static int my_vivid_vidioc_querycap(struct file *file, void  *priv,
-					struct v4l2_capability *cap)
+                                    struct v4l2_capability *cap)
 {
-	printk("enter %s\n", __func__);
-	
-	strcpy(cap->driver, "my_vivid");
-	strcpy(cap->card, "my_vivid");
-	cap->version = 0x0001;
+    printk("enter %s\n", __func__);
 
-	cap->device_caps  = V4L2_CAP_VIDEO_CAPTURE | V4L2_CAP_STREAMING | V4L2_CAP_DEVICE_CAPS;
-	cap->capabilities =	V4L2_CAP_VIDEO_CAPTURE | V4L2_CAP_STREAMING | V4L2_CAP_DEVICE_CAPS;
-	
-	return 0;
+    strcpy(cap->driver, "my_vivid");
+    strcpy(cap->card, "my_vivid");
+    cap->version = 0x0001;
+
+    cap->device_caps  = V4L2_CAP_VIDEO_CAPTURE | V4L2_CAP_STREAMING | V4L2_CAP_DEVICE_CAPS;
+    cap->capabilities =	V4L2_CAP_VIDEO_CAPTURE | V4L2_CAP_STREAMING | V4L2_CAP_DEVICE_CAPS;
+
+    return 0;
 }
 
 static int my_vivid_vidioc_enum_fmt_vid_cap(struct file *file, void  *priv,
-					struct v4l2_fmtdesc *f)
+        struct v4l2_fmtdesc *f)
 {
-	printk("enter %s\n", __func__);
-	
-	if (f->index >= 1)
-		return -EINVAL;
+    printk("enter %s\n", __func__);
 
-	strcpy(f->description, "4:2:2, packed, YUYV");
-	f->pixelformat = V4L2_PIX_FMT_YUYV;
+    if (f->index >= 1)
+        return -EINVAL;
+
+    strcpy(f->description, "4:2:2, packed, YUYV");
+    f->pixelformat = V4L2_PIX_FMT_YUYV;
 
     return 0;
 }
 
 static int my_vivid_vidioc_g_fmt_vid_cap(struct file *file, void *priv,
-					struct v4l2_format *f)
+        struct v4l2_format *f)
 {
-	printk("enter %s\n", __func__);
-	
+    printk("enter %s\n", __func__);
+
     memcpy(f, &my_vivid_format, sizeof(my_vivid_format));
 
     return 0;
-}   
+}
 
 static int my_vivid_vidioc_try_fmt_vid_cap(struct file *file, void *priv,
-			struct v4l2_format *f)
+        struct v4l2_format *f)
 {
-	unsigned int maxw, maxh;
+    unsigned int maxw, maxh;
     enum v4l2_field field;
 
-	printk("enter %s\n", __func__);
-	
+    printk("enter %s\n", __func__);
+
     if (f->fmt.pix.pixelformat != V4L2_PIX_FMT_YUYV)
         return -EINVAL;
 
-	field = f->fmt.pix.field;
+    field = f->fmt.pix.field;
 
-	if (field == V4L2_FIELD_ANY) 
+    if (field == V4L2_FIELD_ANY)
     {
-		field = V4L2_FIELD_INTERLACED;
-	} 
-    else if (V4L2_FIELD_INTERLACED != field) 
+        field = V4L2_FIELD_INTERLACED;
+    }
+    else if (V4L2_FIELD_INTERLACED != field)
     {
-		return -EINVAL;
-	}
+        return -EINVAL;
+    }
 
-	maxw  = 1024;
-	maxh  = 768;
+    maxw  = 1024;
+    maxh  = 768;
 
-    /* 调整format的width, height, 
+    /* 调整format的width, height,
      * 计算bytesperline, sizeimage
      */
-	v4l_bound_align_image(&f->fmt.pix.width, 48, maxw, 2,
-			      &f->fmt.pix.height, 32, maxh, 0, 0);
-	f->fmt.pix.bytesperline =
-		(f->fmt.pix.width * 16) >> 3;
-	f->fmt.pix.sizeimage =
-		f->fmt.pix.height * f->fmt.pix.bytesperline;
+    v4l_bound_align_image(&f->fmt.pix.width, 48, maxw, 2,
+                          &f->fmt.pix.height, 32, maxh, 0, 0);
+    f->fmt.pix.bytesperline =
+        (f->fmt.pix.width * 16) >> 3;
+    f->fmt.pix.sizeimage =
+        f->fmt.pix.height * f->fmt.pix.bytesperline;
 
-	return 0;
-}                  
-            
+    return 0;
+}
+
 static int my_vivid_vidioc_s_fmt_vid_cap(struct file *file, void *priv,
-					struct v4l2_format *f)
+        struct v4l2_format *f)
 {
-	int ret;
-    
+    int ret;
+
     printk("enter %s\n", __func__);
-    
+
     ret = my_vivid_vidioc_try_fmt_vid_cap(file, NULL, f);
 
     if (ret < 0)
-		return ret;
+        return ret;
 
     memcpy(&my_vivid_format, f, sizeof(my_vivid_format));
-    
-	return ret;
+
+    return ret;
 }
 
 static int my_vivid_vidioc_reqbufs(struct file *file, void *priv,
-			  struct v4l2_requestbuffers *p)
+                                   struct v4l2_requestbuffers *p)
 {
-	printk("enter %s\n", __func__);
-    
-	return (videobuf_reqbufs(&my_vivid_vb_vidqueue, p));
-}              
+    printk("enter %s\n", __func__);
+
+    return (videobuf_reqbufs(&my_vivid_vb_vidqueue, p));
+}
 
 static int my_vivid_vidioc_querybuf(struct file *file, void *priv, struct v4l2_buffer *p)
 {
-	printk("enter %s\n", __func__);
-    
-	return (videobuf_querybuf(&my_vivid_vb_vidqueue, p));
-}              
+    printk("enter %s\n", __func__);
+
+    return (videobuf_querybuf(&my_vivid_vb_vidqueue, p));
+}
 
 static int my_vivid_vidioc_qbuf(struct file *file, void *priv, struct v4l2_buffer *p)
 {
-	printk("enter %s\n", __func__);
-    
-	return (videobuf_qbuf(&my_vivid_vb_vidqueue, p));
+    printk("enter %s\n", __func__);
+
+    return (videobuf_qbuf(&my_vivid_vb_vidqueue, p));
 }
 
 static int my_vivid_vidioc_dqbuf(struct file *file, void *priv, struct v4l2_buffer *p)
 {
-	printk("enter %s\n", __func__);
-    
-	return (videobuf_dqbuf(&my_vivid_vb_vidqueue, p, file->f_flags & O_NONBLOCK));
+    printk("enter %s\n", __func__);
+
+    return (videobuf_dqbuf(&my_vivid_vb_vidqueue, p, file->f_flags & O_NONBLOCK));
 }
 
 static int my_vivid_vidioc_streamon(struct file *file, void *priv, enum v4l2_buf_type i)
 {
-	printk("enter %s\n", __func__);
-    
-	return videobuf_streamon(&my_vivid_vb_vidqueue);
+    printk("enter %s\n", __func__);
+
+    return videobuf_streamon(&my_vivid_vb_vidqueue);
 }
 
 static int my_vivid_vidioc_streamoff(struct file *file, void *priv, enum v4l2_buf_type i)
 {
-	printk("enter %s\n", __func__);
-    
-	videobuf_streamoff(&my_vivid_vb_vidqueue);
-    
+    printk("enter %s\n", __func__);
+
+    videobuf_streamoff(&my_vivid_vb_vidqueue);
+
     return 0;
 }
 
 
-static const struct v4l2_ioctl_ops my_vivid_ioctl_ops = {
-        // 表示它是一个摄像头设备
-        .vidioc_querycap          = my_vivid_vidioc_querycap,
+static const struct v4l2_ioctl_ops my_vivid_ioctl_ops =
+{
+    // 表示它是一个摄像头设备
+    .vidioc_querycap          = my_vivid_vidioc_querycap,
 
-        /* 用于列举、获得、测试、设置摄像头的数据的格式 */
-        .vidioc_enum_fmt_vid_cap  = my_vivid_vidioc_enum_fmt_vid_cap,
-        .vidioc_g_fmt_vid_cap     = my_vivid_vidioc_g_fmt_vid_cap,
-        .vidioc_try_fmt_vid_cap   = my_vivid_vidioc_try_fmt_vid_cap,
-        .vidioc_s_fmt_vid_cap     = my_vivid_vidioc_s_fmt_vid_cap,
-        
-        /* 缓冲区操作: 申请/查询/放入队列/取出队列 */
-        .vidioc_reqbufs       = my_vivid_vidioc_reqbufs,
-        .vidioc_querybuf      = my_vivid_vidioc_querybuf,
-        .vidioc_qbuf          = my_vivid_vidioc_qbuf,
-        .vidioc_dqbuf         = my_vivid_vidioc_dqbuf,
-        
-        // 启动/停止
-        .vidioc_streamon      = my_vivid_vidioc_streamon,
-        .vidioc_streamoff     = my_vivid_vidioc_streamoff,   
+    /* 用于列举、获得、测试、设置摄像头的数据的格式 */
+    .vidioc_enum_fmt_vid_cap  = my_vivid_vidioc_enum_fmt_vid_cap,
+    .vidioc_g_fmt_vid_cap     = my_vivid_vidioc_g_fmt_vid_cap,
+    .vidioc_try_fmt_vid_cap   = my_vivid_vidioc_try_fmt_vid_cap,
+    .vidioc_s_fmt_vid_cap     = my_vivid_vidioc_s_fmt_vid_cap,
+
+    /* 缓冲区操作: 申请/查询/放入队列/取出队列 */
+    .vidioc_reqbufs       = my_vivid_vidioc_reqbufs,
+    .vidioc_querybuf      = my_vivid_vidioc_querybuf,
+    .vidioc_qbuf          = my_vivid_vidioc_qbuf,
+    .vidioc_dqbuf         = my_vivid_vidioc_dqbuf,
+
+    // 启动/停止
+    .vidioc_streamon      = my_vivid_vidioc_streamon,
+    .vidioc_streamoff     = my_vivid_vidioc_streamoff,
 };
 
 
-static const struct v4l2_file_operations my_vivid_fops = {
-	.owner		    = THIS_MODULE,
+static const struct v4l2_file_operations my_vivid_fops =
+{
+    .owner		    = THIS_MODULE,
     .open           = my_vivid_open,
     .release        = my_vivid_close,
     .mmap           = my_vivid_mmap,
@@ -389,11 +393,11 @@ static int my_vivid_probe(struct platform_device *pdev)
 
     /* 1.分配一个video_device结构体 */
     my_vivid_dev = video_device_alloc();
-    if (NULL == my_vivid_dev) 
+    if (NULL == my_vivid_dev)
     {
         printk("Failed to alloc video device (%d)\n", ret);
-		return -ENOMEM;
-	}
+        return -ENOMEM;
+    }
 
     /* 2.设置 */
     my_vivid_dev->release   = my_vivid_dev_release;
@@ -403,19 +407,20 @@ static int my_vivid_probe(struct platform_device *pdev)
 
     //队列操作b://定义/初始化一个队列
     spin_lock_init(&my_vivid_queue_slock);
-    
+
     /* 3.注册 */
     ret = video_register_device(my_vivid_dev, VFL_TYPE_GRABBER, -1);
-    if (ret) {
-		printk("Failed to register as video device (%d)\n", ret);
-        goto err_register_dev;		
-	}
+    if (ret)
+    {
+        printk("Failed to register as video device (%d)\n", ret);
+        goto err_register_dev;
+    }
 
     //用定时器产生数据并唤醒进程
-	init_timer(&my_vivid_timer);
+    init_timer(&my_vivid_timer);
     my_vivid_timer.function  = my_vivid_timer_function;
     INIT_LIST_HEAD(&my_vivid_vb_local_queue);
- 
+
     return 0;
 
 err_register_dev:
@@ -438,34 +443,36 @@ static void my_vivid_pdev_release(struct device *dev)
     printk("enter %s\n", __func__);
 }
 
-static struct platform_device my_vivid_pdev = {
-	.name		 = "my_vivid",
-	.dev.release = my_vivid_pdev_release,
+static struct platform_device my_vivid_pdev =
+{
+    .name		 = "my_vivid",
+    .dev.release = my_vivid_pdev_release,
 };
 
-static struct platform_driver my_vivid_pdrv = {
-	.probe		= my_vivid_probe,
-	.remove		= my_vivid_remove,
-	.driver		= {
-		.name	= "my_vivid",
-	},
+static struct platform_driver my_vivid_pdrv =
+{
+    .probe		= my_vivid_probe,
+    .remove		= my_vivid_remove,
+    .driver		= {
+        .name	= "my_vivid",
+    },
 };
 
 static int my_vivid_init(void)
 {
-	int ret;
+    int ret;
 
     printk("enter %s\n", __func__);
 
-	ret = platform_device_register(&my_vivid_pdev);
-	if (ret)
-		return ret;
+    ret = platform_device_register(&my_vivid_pdev);
+    if (ret)
+        return ret;
 
-	ret = platform_driver_register(&my_vivid_pdrv);
-	if (ret)
-		platform_device_unregister(&my_vivid_pdev);
+    ret = platform_driver_register(&my_vivid_pdrv);
+    if (ret)
+        platform_device_unregister(&my_vivid_pdev);
 
-	return ret;
+    return ret;
 
 }
 
@@ -474,7 +481,7 @@ static void my_vivid_exit(void)
     printk("enter %s\n", __func__);
 
     platform_driver_unregister(&my_vivid_pdrv);
-	platform_device_unregister(&my_vivid_pdev);
+    platform_device_unregister(&my_vivid_pdev);
 }
 
 module_init(my_vivid_init);
